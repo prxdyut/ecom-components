976cc477425feb267196a49ba072aa9a11a75cdf
Pradyut Das <daspradyut516@gmail.com>
2023-04-18 14:19:19 +0000

    Update tray.js

--- src/containers/tray.js
+++ src/containers/tray.js
@@ -27,21 +27,93 @@ import { MdExpandMore } from "react-icons/md";
 import { BsPlus, BsDash } from "react-icons/bs";
 import * as API from "../api";
 
+const Product = (props) => {
+  const item = props;
+  const [quantity, setQuantity] = React.useState();
+
+  React.useEffect(() => setQuantity(props.quantity), []);
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+    API.updateCartProductQuantity(props.id, quantity + 1).then((response) =>
+      console.log(response)
+    );
+  };
+
+  const decreaseQuantity = () => {
+    setQuantity(quantity - 1);
+    API.updateCartProductQuantity(props.id, quantity - 1).then((response) =>
+      console.log(response)
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <Card elevation={0}>
+        <CardActionArea
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <AspectRatio ratio="1/1" sx={{ minWidth: "33.3%" }}></AspectRatio>
+          <Box
+            sx={{
+              py: 1,
+              px: 2,
+              flexGrow: 1,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box sx={{ flexGrow: 1 }}>
+              <Typography variant="body1" fontWeight="500">
+                {item.name}
+              </Typography>
+              <Typography variant="body2">Chicken Dum Biryani</Typography>
+              <GiChickenOven style={{ color: "red" }} />
+            </Box>
+          </Box>
+        </CardActionArea>
+        <CardActions>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              alignSelf: "end",
+            }}
+          >
+            <IconButton
+              sx={{ border: 1 }}
+              size="small"
+              onClick={increaseQuantity}
+            >
+              <BsPlus />
+            </IconButton>
+            <Typography variant="body1" sx={{ px: 2 }}>
+              {quantity}
+            </Typography>
+            <IconButton
+              sx={{ border: 1 }}
+              size="small"
+              onClick={decreaseQuantity}
+            >
+              <BsDash />
+            </IconButton>
+          </Box>
+        </CardActions>
+      </Card>
+    </React.Fragment>
+  );
+};
+
 export default function TrayContainer() {
   const [data, setData] = React.useState(null);
 
   API.getCart().then((response) => setData(response));
 
   console.log(data);
-
-  const increaseQuantity = (id, quantity) =>
-    API.updateCartProductQuantity(id, quantity + 1).then((response) =>
-      console.log(response)
-    );
-  const decreaseQuantity = (id, quantity) =>
-    API.updateCartProductQuantity(id, quantity - 1).then((response) =>
-      console.log(response)
-    );
 
   return (
     <>
@@ -53,68 +125,7 @@ export default function TrayContainer() {
         <Stack gap={2}>
           {!!data &&
             data.line_items.map((item, index) => (
-              <React.Fragment key={index}>
-                <Card elevation={0}>
-                  <CardActionArea
-                    sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "flex-start",
-                    }}
-                  >
-                    <AspectRatio
-                      ratio="1/1"
-                      sx={{ minWidth: "33.3%" }}
-                    ></AspectRatio>
-                    <Box
-                      sx={{
-                        py: 1,
-                        px: 2,
-                        flexGrow: 1,
-                        display: "flex",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <Box sx={{ flexGrow: 1 }}>
-                        <Typography variant="body1" fontWeight="500">
-                          {item.name}
-                        </Typography>
-                        <Typography variant="body2">
-                          Chicken Dum Biryani
-                        </Typography>
-                        <GiChickenOven style={{ color: "red" }} />
-                      </Box>
-                    </Box>
-                  </CardActionArea>
-                  <CardActions>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        alignSelf: "end",
-                      }}
-                    >
-                      <IconButton
-                        sx={{ border: 1 }}
-                        size="small"
-                        onClick={() => increaseQuantity(item.id, item.quantity)}
-                      >
-                        <BsPlus />
-                      </IconButton>
-                      <Typography variant="body1" sx={{ px: 2 }}>
-                        {item.quantity}
-                      </Typography>
-                      <IconButton
-                        sx={{ border: 1 }}
-                        size="small"
-                        onClick={() => decreaseQuantity(item.id, item.quantity)}
-                      >
-                        <BsDash />
-                      </IconButton>
-                    </Box>
-                  </CardActions>
-                </Card>
-              </React.Fragment>
+              <Product {...item} key={index} />
             ))}
           <Box></Box>
         </Stack>
